4219c2f4ec016436f9ee086183e4932a23431c76
e <77834992@qq.com>
2019-06-20 11:31:13 +0800

    首页布局头部完成

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>home</div>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row class="row">
+        <el-col :span="4">
+            <img src="@/assets/logo.png" alt="图片加载失败">
+        </el-col>
+        <el-col :span="19">
+          <h1>后台管理系统</h1>
+        </el-col>
+        <el-col :span="1">
+          <a href="">退出</a>
+        </el-col>
+      </el-row>
+    </el-header> 
+    <el-container>
+      <el-aside width="200px" class="aside">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.container {
+  height: 100%;
+  background: #ccc;
+}
+.aside {
+  background: red;
+}
+.main {
+  background-color: yellow;
+}
+h1 {
+    text-align: center;
+    line-height: 60px;
+    color: #fff;
+}
+a {
+    display: block;
+    line-height: 60px;
+    text-decoration: none;
+}
 </style>
