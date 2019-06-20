6a33d2ee4e3c791e62f3bf01e7339abfd47598ec
e <77834992@qq.com>
2019-06-20 18:29:30 +0800

    配置用户路由

--- src/components/home.vue
+++ src/components/home.vue
@@ -21,7 +21,7 @@
               <i class="el-icon-location"></i>
               <span>用户管理</span>
             </template>
-            <el-menu-item index="1-1">用户列表</el-menu-item>
+            <el-menu-item index="users">用户列表</el-menu-item>
           </el-submenu>
           <el-submenu index="2">
             <template slot="title">
@@ -56,7 +56,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+          <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
